dc944f0b3ab10c39091e7055eaf43d2b08988393
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2024-07-01 18:11:39 -0700

    main half

--- am2/main.py
+++ am2/main.py
@@ -1,8 +1,9 @@
 import flet as ft
-import time
+import sql
+from passlib.hash import pbkdf2_sha256
 
-
-def changePage(pageNum: int, page):
+def changePage(pageNum: int, page:ft.Page):
+    # Need Update after this function
     page.controls.clear()
     match pageNum:
         case 0:
@@ -15,21 +16,281 @@ def changePage(pageNum: int, page):
             return Account(page)
         case 4:
             return AddAccount(page)
-        
+        case 5:
+            return Setting(page)
+        case 6:
+            return ChangePW(page)
+
+class CenterCon(ft.Container):
+    """Aligned center container"""
+    def __init__(self,content,margin=0):
+        super().__init__()
+        self.content = content
+        self.margin = margin
+        self.alignment = ft.alignment.center
+
+class AccountBtn(ft.TextButton):
+    def __init__(self, id, text, on_click, data, **kwargs):
+        super().__init__(kwargs)
+        self.id = id
+        self.text = text
+        self.style = ft.ButtonStyle(
+                        color="BLACK",
+                        bgcolor="#45D094",
+                        overlay_color="#1E9690",
+                        shape=ft.RoundedRectangleBorder(radius=10),
+                    )
+        self.width = 250
+        self.height = 100
+        self.data = data
+        self.on_click = on_click
+        self.on_click = lambda e: on_click(e, self.data)
+
+# Pages
 class PW: # 0
     def __init__(self, page: ft.Page):
+        self.db = sql.DynamoDB()
         self.page = page
-        self.page.add(ft.SafeArea(ft.Text("PW")))
-
+        self.hashedPW = self.db.get(id=0)['pw']
+        
+        self.pw_field = ft.TextField(bgcolor="WHITE24",
+                                autofocus=True,
+                                password=True,
+                                multiline=False,
+                                text_align=ft.TextAlign.CENTER,
+                                on_submit=self.enter)
+        
+        self.page.add(
+            ft.Column(
+                spacing = 15,
+                controls=[
+                    CenterCon(
+                        ft.Image("assets/security_icons.png",
+                                width=180),
+                        margin=ft.margin.only(top=40)
+                    ),
+                    CenterCon(
+                        ft.Text("Please enter your password",
+                                size=20),
+                    ),
+                    self.pw_field,
+                    CenterCon(
+                        ft.ElevatedButton(content=ft.Text(value="Enter",size=20),
+                                          color="BLACK",
+                                          bgcolor="#45D094",
+                                          style=ft.ButtonStyle(
+                                              overlay_color="#1E9690",
+                                          ),
+                                          height=35,
+                                          width=100,
+                                          on_click=self.enter),
+                    )
+                ]   
+            )
+        )
+    
+    def enter(self,e):
+        pw = self.pw_field.value
+        
+        if pbkdf2_sha256.verify(pw, self.hashedPW):
+            # Go to Main
+            changePage(2,self.page)
+            self.page.update()
+        else:
+            # open dialog
+            dlg = ft.AlertDialog(title=ft.Text("Wrong Password", 
+                                               size=20, 
+                                               text_align="center"))
+            self.page.open(dlg)
+            
 class PWsetUp: # 1
     def __init__(self, page: ft.Page):
+        self.db = sql.DynamoDB()
         self.page = page
-        self.page.add(ft.SafeArea(ft.Text("PWsetUp")))
+
+        self.pw_field = ft.TextField(bgcolor="WHITE24",
+                                        autofocus=True,
+                                        password=True,
+                                        multiline=False,
+                                        text_align=ft.TextAlign.CENTER,
+                                        on_submit=self.enter)
+        
+        self.pwConfirmField = ft.TextField(bgcolor="WHITE24",
+                                           autofocus=True,
+                                           password=True,
+                                           multiline=False,
+                                           text_align=ft.TextAlign.CENTER,
+                                           on_submit=self.enter)
+
+        self.page.add(
+            ft.Column(
+                spacing=15,
+                controls=[
+                    CenterCon(
+                        ft.Image("assets/security_icons.png",
+                                    width=180),
+                        margin=ft.margin.only(top=40)
+                    ),
+                    CenterCon(
+                        ft.Text("Set Up Your Password",
+                                size=20),
+                    ),
+                    self.pw_field,
+                    self.pwConfirmField,
+                    CenterCon(
+                        ft.ElevatedButton(content=ft.Text(value="Enter", size=20),
+                                            color="BLACK",
+                                            bgcolor="#45D094",
+                                            style=ft.ButtonStyle(
+                            overlay_color="#1E9690",
+                        ),
+                            height=35,
+                            width=100,
+                            on_click=self.enter),
+                    )
+                ]
+            )
+        )
+
+    def enter(self, e):
+        pw = self.pw_field.value
+        pwConfirm = self.pwConfirmField.value
+        print(pw)
+        print(pwConfirm)
+        
+        valid = False
+        
+        if pw == "":
+            alertMsg = "Cannot use empty String"
+        elif pw != pwConfirm:
+            alertMsg = "Passwords Does Not Match"
+            
+        elif pw == pwConfirm:
+            valid = True
+            
+            # hash pw
+            pw = pbkdf2_sha256.hash(pw)
+            
+            # update admin password
+            self.db.update(0,pw=pw)
+            
+            alertMsg = "Password Created"
+        
+        # open dialog
+        dlg = ft.AlertDialog(title=ft.Text(
+            alertMsg, size=20, text_align="center"))
+        self.page.open(dlg)
+        
+        # Go to PW
+        if valid:
+            changePage(0, self.page)
+            self.page.update()
         
 class Main: # 2
     def __init__(self, page: ft.Page):
         self.page = page
-        self.page.add(ft.SafeArea(ft.Text("Main")))
+        self.db = sql.DynamoDB()
+        # get all accounts from DB
+        self.accounts = sorted(self.db.getAll(), key=lambda x: x['name'].lower())
+        
+        self.accountsSubset = self.accounts.copy()
+        
+        self.sortDropDown = ft.Dropdown(width=125,
+                                        label="Sort Order",
+                                        bgcolor="WHITE24",
+                                        options=[
+                                            ft.dropdown.Option(
+                                                "Ascending"),
+                                            ft.dropdown.Option(
+                                                "Descending")
+                                        ],
+                                        value="Ascending",
+                                        on_change=self.sortOrder
+                                        )
+        
+        self.input = ft.TextField(bgcolor="WHITE24",
+                                    autofocus=True,
+                                    multiline=False,
+                                    text_align=ft.TextAlign.LEFT,
+                                    width=150,
+                                    on_submit=self.search)
+        
+        self.AccountButtons = ft.Column(
+            scroll=ft.ScrollMode.ALWAYS,
+        )
+        self.updateAccountButtons()
+        
+        self.ButtonContainer = ft.Container(
+            alignment=ft.alignment.center,
+            width = 400,
+            height = 400*16/9 - 180,
+            margin = ft.margin.only(bottom=100),
+            content=self.AccountButtons
+            )
+        
+        # Page
+        self.page.add(
+            ft.Column(
+                controls=[
+                    # Top Bar  
+                    ft.Container(
+                        margin=ft.margin.only(top=40),
+                        alignment=ft.alignment.top_center,
+                        content=
+                            ft.Row(
+                                spacing=5,
+                                controls=[
+                                    ft.IconButton(icon=ft.icons.SETTINGS_ROUNDED,
+                                                        on_click=self.clickSettings),
+                                    self.sortDropDown,
+                                    self.input,
+                                    ft.IconButton(icon=ft.icons.SEARCH_ROUNDED,
+                                                  on_click=self.search)
+                                ]
+                            )
+                    ),
+                    # Account Buttons
+                    self.ButtonContainer,
+                ]
+            )
+        )
+    
+    def updateAccountButtons(self):
+        self.AccountButtons.controls = [AccountBtn(id=account.get('id'),
+                                                    text=f"{account.get('name')}\n{account.get('account')}\n{
+                                                    account.get('pw')}",
+                                                    on_click=self.clickAccount,
+                                                    data=account)
+                                                    for account in self.accountsSubset]
+                                            
+    def clickSettings(self, e):
+        changePage(5, self.page)
+        self.page.update()
+        
+    def search(self, e):
+        val = self.input.value
+        if val == "":
+            return
+        else:
+            self.accountsSubset = [d for d in self.accounts if val.lower() in d.get('name').lower()]
+        self.sortOrder(e)
+        
+    def sortOrder(self, e):
+        if self.sortDropDown.value == "Ascending":
+            self.accountsSubset = sorted(self.accountsSubset, key=lambda x: x['name'].lower())
+        else:
+            self.accountsSubset = sorted(
+                self.accountsSubset, key=lambda x: x['name'].lower(), reverse=True)
+        
+        self.updateAccountButtons()
+        self.page.update()
+            
+    def clickAccount(self, e, data):
+        print("btn Click")
+        print(data)
+    
+    def clickAdd(self, e):
+        print("ADD")
         
 class Account: # 3
     def __init__(self, page: ft.Page):
@@ -41,18 +302,33 @@ class AddAccount: # 4
         self.page = page
         self.page.add(ft.SafeArea(ft.Text("AddAccount")))
 
-        
+class Setting: # 5
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.page.add(ft.SafeArea(ft.Text("Setting")))
+
+class ChangePW: # 6
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.page.add(ft.SafeArea(ft.Text("ChangePW")))
+
 def main(page: ft.Page):
     page.title = "Account Manager 2"
-    page.window.icon = 
+    page.window.icon = "assets/security_icons.png"
+
     width = 400
     page.window.width = width
     page.window.height = width*16/9
     
-    app = PW(page)
-    # app = changePage(1,page)
-    # page.update()
+    # get admin pw
+    db = sql.DynamoDB()
+    adminPW = db.get(id=0)['pw']
     
-
-
+    # if no pw, set up
+    if adminPW == "":
+        app = PWsetUp(page)
+    else:
+        # app = PW(page)
+        app = Main(page)
+    
 ft.app(main)
---
+++ am2/sql.py
@@ -0,0 +1,182 @@
+import boto3
+import base64
+
+class DynamoDB:
+    def __init__(self) -> None:
+        self.db = boto3.resource('dynamodb')
+        self.table = self.db.Table('Accounts')
+        self. attributes = set(['id', 'name', 'account', 'pw', 'logo', 'note'])
+        
+    def encodeImg(self, img: bytes) -> str:
+        """_summary_
+
+        Encode images with base64
+    
+        Args:
+            img (bytes): image file
+
+        Returns:
+            str: encoded string
+        """
+        return base64.b64encode(img.read()).decode('utf-8')
+
+    def decodeImg(self, img:str) -> bytes:
+        """_summary_
+
+        Decode base64 encoded string to bytes
+        
+        Args:
+            img (str): base64 encoded image
+
+        Returns:
+            bytes: image file
+        """
+        return base64.b64decode(img)
+
+    def count(self) -> int:
+        """_summary_
+
+        Count all items in the table
+        
+        Returns:
+            int: number of items in the table
+        """
+        return self.table.scan(TableName="Accounts", Select='COUNT')["Count"]
+    
+    def put(self, name: str = "", account: str = "", pw: str = "", logo: str = "", note: str = "") -> None:
+        """_summary_
+        
+        Put an item in the table
+        
+        Args:
+            name (str): name for the account
+            account (str): account
+            pw (str): hashed password
+            logo (str): base64 encoded logo image
+            note (str): additional notes about the account
+        """
+        # do nothing if no information is given
+        if name+account+pw+logo+note == "":
+            return 
+        
+        self.table.put_item(
+            Item={
+                'id': self.count(),
+                'account': account,
+                'logo' : logo,
+                'name' : name,
+                'note' : note,
+                'pw'   : pw
+            }
+        )
+        
+    def getAll(self) -> list:
+        """_summary_
+
+        Get all items in the table 
+        Admin Item excluded
+       
+        Returns:
+            list: all items
+        """
+        
+        # get all items
+        accounts = self.table.scan()["Items"]
+        
+        # remove admin item
+        accounts = [d for d in accounts if d.get('id') != 0]
+        
+        return accounts
+    
+    def get(self, **kwargs) -> dict:
+        """_summary_
+
+        Get an item
+
+        Kwrgs:
+                    
+        Returns:
+            list: item
+        """
+        # check if kwargs are valid
+        try:
+            for attribute, value in kwargs.items():
+                if attribute not in self.attributes:
+                    raise NameError
+                if attribute == 'id':
+                    if not isinstance(value, int):
+                        raise TypeError(0,value)
+                elif not isinstance(value, str):
+                    raise TypeError(1, value)
+        except NameError:
+            print("Invalid attribute name.")
+            print("Valid attribute names are 'id', 'name', 'account', 'pw', 'logo', 'note'.")
+        except TypeError as v:
+            if v.args[0] == 0:
+                print("You entered: ", type(v.args[1]))
+                print("Please enter a integer for id")
+            else:
+                print("You entered: ", type(v.args[1]))
+                print("Please enter a string")
+            
+        return self.table.get_item(Key=kwargs)['Item']
+    
+    def update(self, targetID:int, **kwargs)  -> None:
+        """_summary_
+        
+        Update an item
+        
+        Args:
+            targetID (int): item id
+        """
+        if len(kwargs) == 0:
+            return
+        
+        # create expression and value dictionary
+        expression = "SET "
+        values = {}
+        try:
+            for attribute, value in kwargs.items():
+                # validity check
+                if attribute not in self.attributes:
+                    raise NameError
+                if not isinstance(value, str):
+                    raise TypeError(value)
+                if attribute == 'id' and value == 0:
+                    raise ValueError
+                
+                expression += f"{attribute}=:new{attribute},"
+                values[f":new{attribute}"] = value
+            
+            expression = expression[:-1] # get rid of last comma
+
+            # update
+            self.table.update_item(Key={'id': targetID},
+                                   UpdateExpression=expression,
+                                   ExpressionAttributeValues=values
+                                   )
+        except NameError:
+            print("Invalid attribute name.")
+            print(
+                "Valid attribute names are 'id', 'name', 'account', 'pw', 'logo', 'note'.")
+        except TypeError as v:
+            print("You entered: ", type(v.args[0]))
+            print("Please enter a string")
+        except ValueError:
+            print("You cannot update admin item")
+            
+        return
+        
+    def delete(self, targetID:int) -> None:
+        """_summary_
+
+        Delete a item with id
+
+        Args:
+            targetID (int): item id
+        """
+        if targetID == 0:
+            print("ERROR: Do not delete admin item")
+            return 
+        
+        self.table.delete_item(Key={'id': targetID})
